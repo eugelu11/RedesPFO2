3cfc250171e019107af68db4a1eb97c6ab1894b4
eugelu11 <34776849+eugelu11@users.noreply.github.com>
2026-05-01 19:31:05 -0300

    cambiado el cliente para que tareas exija login

--- Cliente.py
+++ Cliente.py
@@ -3,40 +3,52 @@ import requests
 logueado = False
 
 while True:
-    print("1. Registrarse")
-    print("2. Login")
-    if logueado:
-        print("3. Ver tareas")
+    if not logueado:
+        print("1. Registrarse")
+        print("2. Login")
+        print("3. Salir")
+        opcion = input("Elegí una opción: ") #acá no se muestran las tareas porque no se hizo el login
 
-    opcion = input("Elegí una opción: ")
+        if opcion == "1":
+            usuario = input("Usuario: ")
+            contraseña = input("Contraseña: ")
+            res = requests.post("http://127.0.0.1:5000/registro", json={
+                "usuario": usuario,
+                "contraseña": contraseña
+            })
+            print(res.json())
 
-    if opcion == "1":
-        usuario = input("Usuario: ")
-        contraseña = input("Contraseña: ")
-        res = requests.post("http://127.0.0.1:5000/registro", json={
-            "usuario": usuario,
-            "contraseña": contraseña
-        })
-        print(res.json())
+        elif opcion == "2":
+            usuario = input("Usuario: ")
+            contraseña = input("Contraseña: ")
+            res = requests.post("http://127.0.0.1:5000/login", json={
+                "usuario": usuario,
+                "contraseña": contraseña
+            })
+            datos = res.json()
+            print(datos)
+            if datos["mensaje"] == "Login exitoso":
+                logueado = True
 
-    elif opcion == "2":
-        usuario = input("Usuario: ")
-        contraseña = input("Contraseña: ")
-        res = requests.post("http://127.0.0.1:5000/login", json={
-            "usuario": usuario,
-            "contraseña": contraseña
-        })
-        datos = res.json()
-        print(datos)
-        if datos["mensaje"] == "Login exitoso":
-            logueado = True  # recien cuando uno se loguea se habilita la opción de tareas
+        elif opcion == "3":
+            print("Chauuu")
+            break
 
-    elif opcion == "3":
-        if not logueado:
-            print("Tenés que hacer login primero.")
         else:
+            print("Seleccioná una opción válida")
+
+    else:
+        print("1. Ver tareas")
+        print("2. Salir")
+        opcion = input("Elegí una opción: ") #esta opción solo se activa al hacer el login
+
+        if opcion == "1":
             res = requests.get("http://127.0.0.1:5000/tareas")
             print(res.text)
 
-    else:
-        print("Seleccioná una opción válida")
+        elif opcion == "2":
+            print("Chauuu")
+            break
+
+        else:
+            print("Elegí una opción válida")

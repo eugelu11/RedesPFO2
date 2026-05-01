f427cb4b4a4baa58192e48864a601c3f1ffbc74b
eugelu11 <34776849+eugelu11@users.noreply.github.com>
2026-05-01 19:17:44 -0300

    Update Cliente.py

--- Cliente.py
+++ Cliente.py
@@ -1,36 +1,42 @@
 import requests
 
+logueado = False
+
 while True:
     print("1. Registrarse")
     print("2. Login")
-    print("3. Ver tareas")
+    if logueado:
+        print("3. Ver tareas")
 
     opcion = input("Elegí una opción: ")
 
     if opcion == "1":
         usuario = input("Usuario: ")
         contraseña = input("Contraseña: ")
-
         res = requests.post("http://127.0.0.1:5000/registro", json={
-        "usuario": usuario,
-        "contraseña": contraseña
+            "usuario": usuario,
+            "contraseña": contraseña
         })
         print(res.json())
-        
+
     elif opcion == "2":
         usuario = input("Usuario: ")
         contraseña = input("Contraseña: ")
-
         res = requests.post("http://127.0.0.1:5000/login", json={
-        "usuario": usuario,
-        "contraseña": contraseña
-         })
-        print(res.json())
-        
+            "usuario": usuario,
+            "contraseña": contraseña
+        })
+        datos = res.json()
+        print(datos)
+        if datos["mensaje"] == "Login exitoso":
+            logueado = True  # recien cuando uno se loguea se habilita la opción de tareas
 
-    elif opcion == "3": 
-        res=requests.get("http://127.0.0.1:5000/tareas")
-    
-        print(res.text)
+    elif opcion == "3":
+        if not logueado:
+            print("Tenés que hacer login primero.")
+        else:
+            res = requests.get("http://127.0.0.1:5000/tareas")
+            print(res.text)
 
-    else: print("Seleccioná una opción valida")
+    else:
+        print("Seleccioná una opción válida")
